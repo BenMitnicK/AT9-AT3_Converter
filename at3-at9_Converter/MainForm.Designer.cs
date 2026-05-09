a74ecbd27a5dbf4309fc0444109863a1670234db
BenMitnicK <bigboss.bmk@gmail.com>
2026-05-09 18:53:58 +0200

    Update MainForm.Designer.cs

--- at3-at9_Converter/MainForm.Designer.cs
+++ at3-at9_Converter/MainForm.Designer.cs
@@ -64,7 +64,6 @@ namespace at3_at9_Converter
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.comboBoxLanguage = new System.Windows.Forms.ComboBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -83,7 +82,7 @@ namespace at3_at9_Converter
             this.tabControl1.Location = new System.Drawing.Point(1, 1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(401, 256);
+            this.tabControl1.Size = new System.Drawing.Size(410, 331);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -98,7 +97,7 @@ namespace at3_at9_Converter
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage1.Size = new System.Drawing.Size(393, 230);
+            this.tabPage1.Size = new System.Drawing.Size(402, 305);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "AT9Tool PSVita/TV & PS4";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -108,7 +107,7 @@ namespace at3_at9_Converter
             this.button3.Enabled = false;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.DarkRed;
-            this.button3.Location = new System.Drawing.Point(203, 110);
+            this.button3.Location = new System.Drawing.Point(203, 120);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(188, 36);
             this.button3.TabIndex = 9;
@@ -130,7 +129,7 @@ namespace at3_at9_Converter
             this.groupBox1.ForeColor = System.Drawing.Color.White;
             this.groupBox1.Location = new System.Drawing.Point(5, 10);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(192, 136);
+            this.groupBox1.Size = new System.Drawing.Size(192, 146);
             this.groupBox1.TabIndex = 8;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Select Type Convertion";
@@ -259,7 +258,7 @@ namespace at3_at9_Converter
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.label1.Location = new System.Drawing.Point(27, 179);
+            this.label1.Location = new System.Drawing.Point(100, 270);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(196, 25);
             this.label1.TabIndex = 7;
@@ -271,7 +270,7 @@ namespace at3_at9_Converter
             this.button2.Enabled = false;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.DarkRed;
-            this.button2.Location = new System.Drawing.Point(203, 37);
+            this.button2.Location = new System.Drawing.Point(203, 48);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(188, 66);
             this.button2.TabIndex = 6;
@@ -282,7 +281,7 @@ namespace at3_at9_Converter
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(9, 152);
+            this.textBox1.Location = new System.Drawing.Point(9, 243);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(382, 20);
             this.textBox1.TabIndex = 4;
@@ -299,7 +298,7 @@ namespace at3_at9_Converter
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage2.Size = new System.Drawing.Size(393, 230);
+            this.tabPage2.Size = new System.Drawing.Size(402, 305);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "AT3Tool PSP & PS3";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -309,7 +308,7 @@ namespace at3_at9_Converter
             this.button1.Enabled = false;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.DarkRed;
-            this.button1.Location = new System.Drawing.Point(201, 96);
+            this.button1.Location = new System.Drawing.Point(201, 108);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(188, 36);
             this.button1.TabIndex = 17;
@@ -332,7 +331,7 @@ namespace at3_at9_Converter
             this.groupBox2.ForeColor = System.Drawing.Color.White;
             this.groupBox2.Location = new System.Drawing.Point(5, 10);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(192, 122);
+            this.groupBox2.Size = new System.Drawing.Size(192, 146);
             this.groupBox2.TabIndex = 16;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Select Type Convertion";
@@ -342,7 +341,7 @@ namespace at3_at9_Converter
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.InitialImage = null;
-            this.pictureBox2.Location = new System.Drawing.Point(169, 97);
+            this.pictureBox2.Location = new System.Drawing.Point(169, 108);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(19, 14);
             this.pictureBox2.TabIndex = 21;
@@ -353,7 +352,7 @@ namespace at3_at9_Converter
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 68);
+            this.label4.Location = new System.Drawing.Point(8, 79);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(84, 13);
             this.label4.TabIndex = 20;
@@ -363,7 +362,7 @@ namespace at3_at9_Converter
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(8, 96);
+            this.label3.Location = new System.Drawing.Point(8, 107);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(88, 13);
             this.label3.TabIndex = 19;
@@ -375,7 +374,7 @@ namespace at3_at9_Converter
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.Enabled = false;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(99, 93);
+            this.comboBox2.Location = new System.Drawing.Point(99, 104);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(64, 21);
             this.comboBox2.TabIndex = 18;
@@ -386,7 +385,7 @@ namespace at3_at9_Converter
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Enabled = false;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(99, 65);
+            this.comboBox1.Location = new System.Drawing.Point(99, 76);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(64, 21);
             this.comboBox1.TabIndex = 17;
@@ -398,7 +397,7 @@ namespace at3_at9_Converter
             this.radioButton5.Enabled = false;
             this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton5.ForeColor = System.Drawing.Color.White;
-            this.radioButton5.Location = new System.Drawing.Point(6, 16);
+            this.radioButton5.Location = new System.Drawing.Point(6, 27);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(93, 17);
             this.radioButton5.TabIndex = 11;
@@ -414,7 +413,7 @@ namespace at3_at9_Converter
             this.radioButton6.Enabled = false;
             this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton6.ForeColor = System.Drawing.Color.White;
-            this.radioButton6.Location = new System.Drawing.Point(99, 16);
+            this.radioButton6.Location = new System.Drawing.Point(99, 27);
             this.radioButton6.Name = "radioButton6";
             this.radioButton6.Size = new System.Drawing.Size(93, 17);
             this.radioButton6.TabIndex = 10;
@@ -430,7 +429,7 @@ namespace at3_at9_Converter
             this.radioButton7.Enabled = false;
             this.radioButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton7.ForeColor = System.Drawing.Color.White;
-            this.radioButton7.Location = new System.Drawing.Point(6, 42);
+            this.radioButton7.Location = new System.Drawing.Point(6, 53);
             this.radioButton7.Name = "radioButton7";
             this.radioButton7.Size = new System.Drawing.Size(92, 17);
             this.radioButton7.TabIndex = 9;
@@ -446,7 +445,7 @@ namespace at3_at9_Converter
             this.radioButton8.Enabled = false;
             this.radioButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.radioButton8.ForeColor = System.Drawing.Color.White;
-            this.radioButton8.Location = new System.Drawing.Point(99, 42);
+            this.radioButton8.Location = new System.Drawing.Point(99, 53);
             this.radioButton8.Name = "radioButton8";
             this.radioButton8.Size = new System.Drawing.Size(92, 17);
             this.radioButton8.TabIndex = 8;
@@ -461,7 +460,7 @@ namespace at3_at9_Converter
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.DarkSlateGray;
-            this.label2.Location = new System.Drawing.Point(27, 179);
+            this.label2.Location = new System.Drawing.Point(100, 270);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(196, 25);
             this.label2.TabIndex = 15;
@@ -473,7 +472,7 @@ namespace at3_at9_Converter
             this.button4.Enabled = false;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.DarkRed;
-            this.button4.Location = new System.Drawing.Point(201, 20);
+            this.button4.Location = new System.Drawing.Point(201, 26);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(188, 76);
             this.button4.TabIndex = 14;
@@ -484,7 +483,7 @@ namespace at3_at9_Converter
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(5, 138);
+            this.textBox2.Location = new System.Drawing.Point(9, 243);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(382, 20);
             this.textBox2.TabIndex = 12;
@@ -501,9 +500,9 @@ namespace at3_at9_Converter
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
             this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
-            this.statusStrip1.Location = new System.Drawing.Point(0, 255);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 335);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(400, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(410, 22);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
@@ -516,22 +515,10 @@ namespace at3_at9_Converter
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(45, 17);
             this.toolStripStatusLabel1.Text = "Ready!";
             // 
-            // linkLabel2
-            // 
-            this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel2.Location = new System.Drawing.Point(313, 260);
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(87, 13);
-            this.linkLabel2.TabIndex = 3;
-            this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "BMK PortFolio";
-            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
-            // 
             // comboBoxLanguage
             // 
             this.comboBoxLanguage.FormattingEnabled = true;
-            this.comboBoxLanguage.Location = new System.Drawing.Point(203, 10);
+            this.comboBoxLanguage.Location = new System.Drawing.Point(203, 16);
             this.comboBoxLanguage.Name = "comboBoxLanguage";
             this.comboBoxLanguage.Size = new System.Drawing.Size(188, 21);
             this.comboBoxLanguage.TabIndex = 10;
@@ -541,8 +528,7 @@ namespace at3_at9_Converter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 277);
-            this.Controls.Add(this.linkLabel2);
+            this.ClientSize = new System.Drawing.Size(410, 357);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -600,7 +586,6 @@ namespace at3_at9_Converter
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.LinkLabel linkLabel2;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.Button button1;
